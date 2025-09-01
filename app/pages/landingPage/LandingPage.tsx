faf0b7dab5f0bbcbece9aee6ee30522c90d6efce
Asfar Waheed <asfarwaheedch123@gmail.com>
2025-09-01 14:38:21 +0500

    initial commit

---
+++ app/pages/landingPage/LandingPage.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/app/components/Layout/Navbar/Navbar";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default LandingPage;
