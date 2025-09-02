945e3f1a50afe6540268365c77d703a92370fbd8
Asfar Waheed <asfarwaheedch123@gmail.com>
2025-09-02 21:02:30 +0500

    Removed dynamic Imports

--- app/pages/landingPage/LandingPage.tsx
+++ app/pages/landingPage/LandingPage.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/app/components/LandingPage/HeroSection/HeroSection";
 import Navbar from "@/app/components/Layout/Navbar/Navbar";
 import React from "react";
-import dynamic from "next/dynamic";
 import AboutusSection from "@/app/components/LandingPage/AboutusSection/AboutusSection";
 import BenefitsSection from "@/app/components/LandingPage/BenefitsSection/BenefitsSection";
 import PartnersSection from "@/app/components/LandingPage/PartnersSection/PartnersSection";
